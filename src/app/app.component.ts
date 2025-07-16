8fd764870e50198de28ac08a9eb34027a481d42b
Yasha <karpov.yasha@mail.ru>
2025-07-16 13:50:06 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {FormComponent} from "./form/form.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
